d57beaa953e684dc0c2c536499d95d90f494f743
Marco Mirandilla <rcmirandilla@up.edu.ph>
2023-07-13 13:53:47 +0800

    set up navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
 
+import Root from "./modules/Root";
+import Login from "./modules/accounts/pages/Login";
+import GetAllTerritories from "./modules/territories/pages/GetAllTerritories";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <GetAllTerritories />,
+      },
+      {
+        path: "/accounts/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>This is App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
